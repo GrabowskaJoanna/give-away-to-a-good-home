ec44697007a136ff663e96f967e0c823db3abc72
GrabowskaJoanna <joanna.trojanek18@gmail.com>
2024-07-06 17:45:24 +0200

    Added validation to login form and registration form

--- src/abstract/Input.js
+++ src/abstract/Input.js
@@ -1,11 +1,26 @@
 import React from "react";
 
-const Input = ({ text, placeholder, type }) => {
+const Input = ({
+  text,
+  placeholder,
+  type,
+  value,
+  error,
+  onChange,
+  className,
+}) => {
   return (
     <label>
       {" "}
       {text}
-      <input type={type} placeholder={placeholder} />
+      <input
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        className={className}
+      />
+      {error && <p className="text_error">{error}</p>}
     </label>
   );
 };

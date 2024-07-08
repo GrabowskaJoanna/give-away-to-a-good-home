4cb9fbd46f7299c8660290a975bbfbc12b13644f
GrabowskaJoanna <joanna.trojanek18@gmail.com>
2024-07-08 17:31:32 +0200

    Added validation to contact form

--- src/abstract/TextArea.js
+++ src/abstract/TextArea.js
@@ -1,11 +1,17 @@
 import React from "react";
 
-const TextArea = ({ text, placeholder }) => {
+const TextArea = ({ text, placeholder, value, onChange, error, className }) => {
   return (
     <label>
       {" "}
       {text}
-      <textarea placeholder={placeholder} />
+      <textarea
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        className={className}
+      />
+      {error && <p className="text_error">{error}</p>}
     </label>
   );
 };
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Element } from "react-scroll";
 import Decoration from "../abstract/Decoration";
 import Input from "../abstract/Input";
@@ -8,12 +8,59 @@ import fbIcon from "../assets/images/Facebook.png";
 import instagramIcon from "../assets/images/Instagram.png";
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [errors, setErrors] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+
+  const validator = () => {
+    let isValid = true;
+    let error = {};
+    if (!name) {
+      error.name = "Należy podać imię";
+      isValid = false;
+    } else {
+      const regex = /^[^\s]+$/;
+      if (!regex.test(name)) {
+        error.name = "Należy wprowadzić tylko jeden wyraz";
+        isValid = false;
+      }
+    }
+    if (!email) {
+      error.email = "Email jest wymagany";
+      isValid = false;
+    } else {
+      const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!regex.test(email)) {
+        error.email = "Niepoprawny format email";
+        isValid = false;
+      }
+    }
+    if (message.length < 121) {
+      error.message = "Wiadomość musi mieć co najmniej 120 znaków";
+      isValid = false;
+    }
+    setErrors(error);
+    return isValid;
+  };
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   if (validator()) {
+  //     console.log("Walidacja przebiegła pomyślnie");
+  //     fetch("https://fer-api.coderslab.pl/v1/portfolio/contact", {
+  //       method: "POST",
+  //       headers: { "Content-Type": "application/json" },
+  //     });
+  //   }
+  // };
   return (
     <Element name="contactSection" id="contactSection">
       <div className="container">
         <section className="contact_container">
           <div className="contact_img" />
-          <div className="contact_form">
+          <form className="contact_form" onSubmit={handleSubmit}>
             <h1 className="contact_form_header">Skontaktuj się z nami</h1>
             <Decoration />
             <div className="contact_form_inputs">
@@ -21,20 +68,32 @@ const Contact = () => {
                 text="Wpisz swoje imię"
                 type="text"
                 placeholder="Krzysztof"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                error={errors.name}
+                className={errors.name ? "input_error" : ""}
               />
               <Input
                 text="Wpisz swój email"
                 type="email"
                 placeholder="abc@xyz.pl"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                error={errors.email}
+                className={errors.email ? "input_error" : ""}
               />
             </div>
             <TextArea
               text="Wpisz swoją wiadomosć"
               placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
             exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              error={errors.message}
+              className={errors.message ? "textarea_error" : ""}
             />
-            <Button text="Wyślij" />
-          </div>
+            <Button text="Wyślij" type="submit" />
+          </form>
         </section>
         <footer className="footer">
           <p className="footer_text">Copyright by Coders Lab</p>
